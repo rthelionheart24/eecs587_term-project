18e00eb936d89b45a6531bd7edf728dc9c2a8f18
= <=>
2023-12-02 21:03:17 -0500

    Working on reduce

--- multi-shot.cu
+++ multi-shot.cu
@@ -1,124 +1,176 @@
+#include <math.h>
 #include <random>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unordered_map>
 #include <vector>
 
+#include <curand.h>
 #include <curand_kernel.h>
+
+#include "device_launch_parameters.h"
+#include <cuda.h>
+#include <cuda_runtime_api.h>
 
 #define THREADS_PER_BLOCK 1024
 
+enum struct Parameter { ZERO, ONE, ID, RAND };
 
+__managed__ int RAND_POSSIBLE_OUTCOME = 2;
+__managed__ int NUM_PARAMETERS = 3;
+__managed__ int NUM_RANDOM_PARAMETERS = 3;
+__managed__ int NUM_SHOTS = 4;
 
-enum struct Parameter { ADD, SUB, MULT, DIV, RAND };
+__managed__ uint64_t STATE_COUNTER = 1;
 
-typedef struct QData {
+__managed__ float DISTRIBUTION[2];
+
+__managed__ Parameter **params;
+
+typedef struct State {
   float a;
-  float b;
-} QData;
+} State;
 
-typedef struct Task {
-  std::vector<Parameter> params;
-  QData data;
+typedef struct BatchedTask {
+  std::vector<std::vector<Parameter>> params;
+  std::vector<State> states;
   uint64_t num_shots;
-} Task;
 
+} BatchedTask;
 
-__managed__ uint64_t statesCounter = -1;
+__global__ void run(State *states, uint64_t num_shots, int param_idx) {
 
-// __managed__ Parameter* params;
+  uint64_t global_idx = blockIdx.x * blockDim.x + threadIdx.x;
+  uint64_t batch_idx = global_idx / num_shots;
+  uint64_t shot_idx = global_idx % num_shots;
 
-__device__ void printParam(Parameter *param, uint64_t paramsCounter) {
+  if (threadIdx.x == 0) {
+    printf("Waiting to be transformed by parameter %d\n", param_idx);
+  }
 
-  switch (*param) {
+  // Out of bounds
+  if (global_idx >= num_shots * RAND_POSSIBLE_OUTCOME * NUM_RANDOM_PARAMETERS) {
+    return;
+  }
 
-  case Parameter::ADD:
-    printf("Parameter: ADD\n");
-    break;
-  case Parameter::SUB:
-    printf("Parameter: SUB\n");
-    break;
-  case Parameter::MULT:
-    printf("Parameter: MULT\n");
-    break;
-  case Parameter::DIV:
-    printf("Parameter: DIV\n");
-    break;
-  case Parameter::RAND:
-    printf("Parameter: RAND\n");
-    break;
+  // Transformation
+  if (params[shot_idx][param_idx] == Parameter::ZERO) {
+
+    if (global_idx < num_shots * STATE_COUNTER) {
+      states[global_idx].a = 0.0;
+      printf("Setting shot %lu in batch %lu to 0\n", shot_idx, batch_idx);
+    }
+
+  } else if (params[shot_idx][param_idx] == Parameter::ONE) {
+
+    if (global_idx < num_shots * STATE_COUNTER) {
+      states[global_idx].a = 1.0;
+      printf("Setting shot %lu in batch %lu to 0\n", shot_idx, batch_idx);
+    }
+  } else if (params[shot_idx][param_idx] == Parameter::RAND) {
+
+    if (global_idx < num_shots * STATE_COUNTER) {
+
+      for (int i = 0; i < RAND_POSSIBLE_OUTCOME; i++) {
+        states[global_idx + i * num_shots * STATE_COUNTER].a = DISTRIBUTION[i];
+      }
+    }
+
+    STATE_COUNTER *= RAND_POSSIBLE_OUTCOME;
+
+    __syncthreads();
+
+    if (global_idx < num_shots * STATE_COUNTER) {
+      printf("global_idx %lu, value: %f\n", global_idx, states[global_idx].a);
+    }
   }
 }
 
-__device__ void printData(QData *data, uint64_t numBlocksParams) {
-  uint64_t idx = blockIdx.x * blockDim.x + threadIdx.x;
-  uint64_t data_idx = idx - numBlocksParams * THREADS_PER_BLOCK;
+__global__ void reduce(State *states, uint64_t num_shots) {
+  uint64_t global_idx = blockIdx.x * blockDim.x + threadIdx.x;
+  uint64_t shot_idx = global_idx % num_shots;
+  uint64_t batch_idx = global_idx / num_shots;
 
-  printf("Data at position %lu: (%f, %f)\n", idx, data[data_idx].a,
-         data[data_idx].b);
+  extern __shared__ State per_shot_data[];
+
+  if (global_idx >= STATE_COUNTER * num_shots) {
+    return;
+  }
+
+  if (blockIdx.x == shot_idx) {
+    for (int i = 0; i < RAND_POSSIBLE_OUTCOME ; i++) {
+      per_shot_data[i] = states[global_idx + i * num_shots * STATE_COUNTER];
+    }
+  }
+
+  __syncthreads();
+
+  states[threadIdx.x] = per_shot_data[threadIdx.x];
+
+  __syncthreads();
+
+  if (threadIdx.x < STATE_COUNTER) {
+    printf("Experiment %u, shot %lu: %f\n", blockIdx.x, shot_idx,
+           states[threadIdx.x].a);
+  }
 }
 
-__global__ void run(Parameter *params, QData *data, uint64_t num_shots,
-                    uint64_t num_params, uint64_t numBlocksParams,
-                    uint64_t numBlocksData, uint64_t data_idx,
-                    uint64_t params_idx,
-                    uint64_t paramsCounter) {
+__global__ void getCount(State *states, uint64_t num_shots) {
 
   uint64_t global_idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-  if (global_idx < data_idx) {
+  uint64_t result;
+
+  result = states[global_idx].a;
+
+  extern __shared__ State counts[];
+
+  if (threadIdx.x < RAND_POSSIBLE_OUTCOME) {
+    counts[threadIdx.x].a = 0;
+  } else {
     return;
   }
 
-  if (global_idx > data_idx + statesCounter) {
-    return;
+  __syncthreads();
+
+  atomicAdd(&counts[result].a, 1);
+
+  __syncthreads();
+
+  if (threadIdx.x < RAND_POSSIBLE_OUTCOME) {
+    states[threadIdx.x] = counts[threadIdx.x];
   }
 
-  printParam(&params[paramsCounter], paramsCounter);
-  printData(data, numBlocksParams);
-
-  // Transformation
-  if (params[paramsCounter] == Parameter::ADD) {
-    data[data_idx].a += data[data_idx].b;
-  } else if (params[paramsCounter] == Parameter::SUB) {
-    data[data_idx].a -= data[data_idx].b;
-  } else if (params[paramsCounter] == Parameter::MULT) {
-    data[data_idx].a *= data[data_idx].b;
-  } else if (params[paramsCounter] == Parameter::DIV) {
-    // Ensure that data[data_idx].b is not zero to avoid division by zero
-    if (data[data_idx].b != 0) {
-      data[data_idx].a /= data[data_idx].b;
-    }
-  } else if (params[paramsCounter] == Parameter::RAND) {
-    // Note: std::rand() is generally not recommended in CUDA kernels
-    // Consider using curand for random number generation in CUDA
-  
-
-
-  }
-
-
-
-  return;
+  __syncthreads();
 }
 
 int main(int argc, char **argv) {
 
-  QData d{1.0, 3.0};
-  std::vector<Parameter> p{Parameter::ADD, Parameter::SUB, Parameter::MULT,
-                           Parameter::DIV, Parameter::RAND};
+  // =================================================================================================================\
+  // Setting up task
 
-  Task *task;
-  task = (Task *)malloc(sizeof(Task));
-  task->params = p;
-  task->data = d;
+  for (int i = 0; i < RAND_POSSIBLE_OUTCOME; i++) {
+    DISTRIBUTION[i] = i * 10;
+    printf("Distribution %d: %f\n", i, DISTRIBUTION[i]);
+  }
 
-  uint64_t params_idx = 0, data_idx;
+  BatchedTask task;
+  task.num_shots = NUM_SHOTS;
 
-  Parameter *params_ptr;
-  QData *data_ptr;
+  for (int i = 0; i < NUM_SHOTS; i++) {
+    task.params.push_back({Parameter::RAND, Parameter::RAND, Parameter::RAND});
+  }
 
-  uint64_t numBlocksParams = 1, numBlocksData;
+  for (int i = 0; i < NUM_SHOTS; i++) {
+    task.states.push_back({0.0});
+  }
+
+  State *states_ptr;
+
+  printf("Number of shots: %lu\n", task.num_shots);
+  printf("Number of parameters: %d\n", NUM_PARAMETERS);
+  printf("Number of possible outcomes: %d\n", RAND_POSSIBLE_OUTCOME);
 
   // =================================================================================================================
   // Check for CUDA device
@@ -156,107 +208,124 @@ int main(int argc, char **argv) {
 
   cudaDeviceReset();
 
-  curandGenerator_t gen;
-  curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
-  curandSetPseudoRandomGeneratorSeed(gen, 1234ULL);  // Setting a seed for reproducibility
-
+  printf("Number of random parameters: %d\n", NUM_RANDOM_PARAMETERS);
 
   dim3 gridDim;
-  gridDim.x = 0;
 
   cudaError_t status;
-
-  // printf("Start allocating for params at location: %f\n", d_params);
-  status =
-      cudaMalloc((void **)&params_ptr, sizeof(Parameter) * task->params.size());
-  if (status != cudaSuccess) {
-    printf("Error allocating memory for params - %s\n",
-           cudaGetErrorString(status));
-    exit(EXIT_FAILURE);
-  } else {
-    printf("Allocated memory for params\n");
-  }
-
-  gridDim.x += task->params.size() / THREADS_PER_BLOCK;
-  if (task->params.size() % THREADS_PER_BLOCK != 0) {
-    gridDim.x++;
-  }
 
   //// dataOriginal = paramsOriginal + gridDim.x * THREADS_PER_BLOCK;
   //// printf("Start allocating memory for data at location: %f\n",
   /// dataOriginal);
 
-  status = cudaMalloc((void **)&data_ptr, sizeof(QData));
+  status = cudaMalloc((void **)&states_ptr, sizeof(State) * task.num_shots *
+                                                NUM_RANDOM_PARAMETERS *
+                                                RAND_POSSIBLE_OUTCOME);
   if (status != cudaSuccess) {
-    printf("Error allocating memory for data - %s\n",
+    printf("Error allocating memory for initial states - %s\n",
            cudaGetErrorString(status));
     exit(EXIT_FAILURE);
   } else {
-    printf("Allocated memory for data\n");
+    printf("Allocated memory for initial states\n");
   }
 
-  gridDim.x++;
-
+  gridDim.x = task.num_shots * NUM_RANDOM_PARAMETERS * RAND_POSSIBLE_OUTCOME /
+              THREADS_PER_BLOCK;
+  if (task.num_shots * NUM_RANDOM_PARAMETERS * RAND_POSSIBLE_OUTCOME %
+          THREADS_PER_BLOCK !=
+      0) {
+    gridDim.x++;
+  }
+  // TODO: fix kernel
   printf("Grid dim: %d\n", gridDim.x);
   printf("Block dim: %d\n", THREADS_PER_BLOCK);
 
-  data_idx = numBlocksParams * THREADS_PER_BLOCK;
+  cudaMallocManaged((void **)&params, sizeof(Parameter *) * task.num_shots);
+  for (int i = 0; i < task.num_shots; i++) {
+    cudaMallocManaged((void **)&params[i], sizeof(Parameter) * NUM_PARAMETERS);
+
+    for (int j = 0; j < task.params[0].size(); j++) {
+      params[i][j] = task.params[i][j];
+      printf("Shot %d, parameter %d: %d\n", i, j, params[i][j]);
+    }
+  }
 
   // =================================================================================================================
   // Copy data to device
 
-  status = cudaMemcpy((void *)params_ptr, task->params.data(),
-                      sizeof(Parameter) * task->params.size(),
-                      cudaMemcpyHostToDevice);
-  if (status != cudaSuccess) {
-    printf("Error copying params to device - %s\n", cudaGetErrorString(status));
-    exit(EXIT_FAILURE);
-  } else {
-    printf("Copied params of size %zu to device\n", task->params.size());
-  }
+  // status = cudaMemcpy((void *)states_ptr, &(task.state),
+  //                     sizeof(State) * task.num_shots * RAND_POSSIBLE_OUTCOME
+  //                     *
+  //                         NUM_RANDOM_PARAMETERS,
+  //                     cudaMemcpyHostToDevice);
+  // if (status != cudaSuccess) {
+  //   printf("Error copying initial states to device - %s\n",
+  //          cudaGetErrorString(status));
+  //   exit(EXIT_FAILURE);
+  // } else {
+  //   printf("Copied initial states to device\n");
+  // }
 
-  status = cudaMemcpy((void *)data_ptr, &(task->data), sizeof(QData),
-                      cudaMemcpyHostToDevice);
-  if (status != cudaSuccess) {
-    printf("Error copying data to device - %s\n", cudaGetErrorString(status));
-    exit(EXIT_FAILURE);
-  } else {
-    printf("Copied data of length to device\n");
-  }
+  printf("State counter initially: %lu\n", STATE_COUNTER);
 
   // =================================================================================================================
   // Running task
 
   printf("Running task\n");
 
-  statesCounter = 0;
-  uint64_t paramsCounter = 0;
+  for (uint64_t i = 0; i < NUM_PARAMETERS; i++) {
 
-  for (int i = 0; i < task->params.size(); i++) {
-    if (task->params[i] != Parameter::RAND) {
-          // Run the simulation kernel
-    run<<<gridDim, THREADS_PER_BLOCK>>>(params_ptr, data_ptr, task->num_shots,
-                                        task->params.size(), numBlocksParams,
-                                        numBlocksData, data_idx, params_idx,
-                                        paramsCounter);
-    }
-    else{
-      statesCounter++;
-
-      
-
-
-    }
-
-
+    run<<<gridDim, THREADS_PER_BLOCK>>>(states_ptr, task.num_shots, i);
 
     cudaDeviceSynchronize();
+
+    printf("Finish executing parameter %lu\n", i);
+
+    // State temp[task.num_shots * STATE_COUNTER];
+    // cudaMemcpy((void *)&temp, (void *)states_ptr,
+    //            sizeof(State) * task.num_shots * STATE_COUNTER,
+    //            cudaMemcpyDeviceToHost);
+
+    // for (int i = 0; i < task.num_shots*STATE_COUNTER; i++) {
+    //   size_t state_idx = i / task.num_shots;
+    //   size_t shot_idx = i % task.num_shots;
+    //   printf("Shot %lu, state %lu: %f\n", shot_idx, state_idx, temp[i].a);
+    // }
   }
+
+  printf("Start reducing\n");
+  reduce<<<task.num_shots, THREADS_PER_BLOCK,
+           sizeof(State) * THREADS_PER_BLOCK>>>(states_ptr, task.num_shots);
+
+  cudaDeviceSynchronize();
+
+  printf("Start gathering final counts\n");
+  getCount<<<task.num_shots, THREADS_PER_BLOCK>>>(states_ptr, task.num_shots);
+
+  cudaDeviceSynchronize();
+
+  State final_results[task.num_shots * RAND_POSSIBLE_OUTCOME];
+
+  cudaMemcpy((void *)&final_results, (void *)states_ptr,
+             sizeof(State) * task.num_shots * RAND_POSSIBLE_OUTCOME,
+             cudaMemcpyDeviceToHost);
+
+  for (int s = 0; s < task.num_shots; s++) {
+    printf("*******************************************************************"
+           "*\n");
+    for (int j = 0; j < RAND_POSSIBLE_OUTCOME; j++) {
+      printf("Shot %d, outcome %d: %f\n", s, j,
+             final_results[s * RAND_POSSIBLE_OUTCOME + j].a);
+    }
+    printf("*******************************************************************"
+           "*\n");
+  }
+
+  // =================================================================================================================
+  // Printing stats
 
   // =================================================================================================================
   // Benchmarking
 
-  cudaFree(params_ptr);
-  cudaFree(data_ptr);
-  free(task);
+  cudaFree(states_ptr);
 }
